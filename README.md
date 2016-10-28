8849c4ebde513459dcef19754298c6692492f7c3
Marty Kovach <tyedye105@yahoo.com>
2016-10-28 14:02:06 -0700

    add checkbox forms for size, cheese, sauce, and toppings. add first function to determine price of pizaa.

--- README.md
+++ README.md
@@ -7,5 +7,3 @@ Allow user to choose toppings and size of pizza
 Create a pizza object constructor with properties for toppings and size
 
 create a prototype for the cost of a pizza depending on selections chosen.
-
-BDD
--- js/scripts.js
+++ js/scripts.js
@@ -7,14 +7,40 @@
    pie:[]
  }
 
-pizza.size = prompt("Please select a size");
+ var pizzaPrice = function() {
+  if (pizza.size === "small") {
+    pizza.price =+ 4.99;
+  } else if (pizza.size === "medium") {
+    pizza.price =+ 5.99;
+  } else if (pizza.size === "large") {
+    pizza.price =+ 6.99
+    }
 
-pizza.cheese = prompt("please select your cheeses")
 
-pizza.sauce = prompt("Please select a sauce")
+}
 
-pizza.toppings = prompt("pease select your toppings")
 
-pizza.pie.push(pizza.size, pizza.cheese);
 
-alert(pizza.pie);
+// pizza.size = prompt("Please select a size");
+//
+// pizza.cheese = prompt("please select your cheeses")
+//
+// pizza.sauce = prompt("Please select a sauce")
+//
+// pizza.toppings = prompt("pease select your toppings")
+//
+// pizza.pie.push(pizza.size, pizza.cheese);
+//
+// pizzaPrice(pizza.pie);
+//
+// alert(pizza.price);
+$(document).ready(function() {
+  $("form#pizza-choice").submit(function(event){
+    event.preventDefault();
+    pizza.size = $("input:checkbox[ name=p-Size]:checked").val();
+    alert(pizza.size);
+    pizzaPrice(pizza.size);
+    alert(pizza.price);
+
+  });
+});
