602488f4986219072a3de5525015ea0ee8824810
Marty Kovach <tyedye105@yahoo.com>
2016-10-29 11:15:09 -0700

    clean up indentation,spacing,and errant comments.Add readme.

--- README.md
+++ README.md
@@ -1,9 +1,74 @@
 
+#Plentiful Pizza
 
-SPecs
+####A webpage that creates pizza objects from information taken from checkboxes! 10/29/16
 
-Allow user to choose toppings and size of pizza
+#### By _Marty Kovach_
 
-Create a pizza object constructor with properties for toppings and size
+## Description
 
-create a prototype for the cost of a pizza depending on selections chosen.
+_This website takes the user inputs for pizza size, sauce, cheeses, and toppings, and then using a pizza object, and several prototypes gives the user a price for their pizza._
+
+###Specifications in 'plain English'
+
+User can select size of pizza:
+  Example Input:  "small" is checked
+  Example Output: pizza size = "small"
+
+User can select the sauce for the pizza:
+  Example Input:  "red sauce" is checked
+  Example Output: pizza sauce = "red sauce"
+
+User can select cheese for the pizza:
+  Example Input:  "mozzerella" is checked
+  Example Output: pizza cheeses = "mozzerella"
+
+User can select more than one cheeses for the pizza:
+  Example Input:  "mozzerella" and "smoked cheddar" are checked
+  Example Output: pizza cheeses = "mozzerella", "smoked cheddar"
+
+User can select a topping for the pizza:
+  Example Input:  "pepperoni" is checked
+  Example Output: pizza topping = "pepperoni"
+
+User can select as many topping for the pizza as they want:
+  Example Input:  "pepperoni", "sausage", and "diced tomatoes" are checked
+  Example Output: pizza toppings  =  "pepperoni", "sausage", and "diced tomatoes"
+
+When user submits information a new pizza object is created:
+  Example Input: pizza order small, red sauce, mozzerella, pepperoni
+  Example Output: New Pizza {
+    size:small;
+    sauce:red sauce;
+    cheese:mozzerella
+    toppings:pepperoni
+  }
+Using the pizza object, the price of the pizza is calculated with several prototypes, and then is displayed for the user:
+  Example input: pizza.pizzaPrice()
+
+  Example Output: Your Pizza will be: $6.49
+## Setup/Installation Requirements
+
+* Clone this repository
+* host it!  
+    Or
+* Click the link for the live page on github pages!
+https://tyedye105.github.io/pingpong
+
+
+## Known Bugs
+
+selecting multiple sizes, or sauces will cheat the system. DON'T cheat.
+
+## Support and contact details
+
+_For problems, or inquiries please contact me at tyedye105@yahoo.com_
+
+## Technologies Used
+HTML, Bootstrap, CSS, images are copyright to their respective owners
+
+### License
+
+*MIT*
+
+Copyright (c) 2016 **_Marty Kovach_**
--- js/scripts.js
+++ js/scripts.js
@@ -1,3 +1,4 @@
+// back end
 function Pizza(size, sauce, cheese , toppings, price, pie) {
    this.size = size;
    this.sauce = sauce;
@@ -6,34 +7,35 @@ function Pizza(size, sauce, cheese , toppings, price, pie) {
    this.price = 0;
    this.pie = [];
  }
+//takes selected size and calculates a dollar value
  Pizza.prototype.sizeUp = function () {
-   if (this.size === "small") {
-     this.pie.push(4.99);
+  if (this.size === "small") {
+    this.pie.push(4.99);
   } else if (this.size === "medium") {
     this.pie.push(5.99);
-   } else if (this.size === "large") {
-  this.pie.push(6.99);
-} else {
-  this.pie.push(0);
-}
-
+  } else if (this.size === "large") {
+    this.pie.push(6.99);
+  } else {
+    this.pie.push(0);
+  }
 };
+//takes the selected cheeses and calculates a dollar value
 Pizza.prototype.cheeseUp = function () {
   var cheeseFee = 0;
   this.cheese.forEach(function(cheese) {
-    if (cheese === "mozzerella"){
-      return  cheeseFee += .50
-    } else if (cheese === "pepper jack" || cheese === "cheddar") {
-      return cheeseFee += .70
+  if (cheese === "mozzerella"){
+    return  cheeseFee += .50
+  } else if (cheese === "pepper jack" || cheese === "cheddar") {
+    return cheeseFee += .70
   }else if (cheese === "smoked cheddar") {
     return cheeseFee += 1.50
   } else {
     return cheeseFee += 0;
   }
-  })
-  this.pie.push(cheeseFee);
+  });
+this.pie.push(cheeseFee);
 };
-
+//takes selected sauce and calculates a dollar value
 Pizza.prototype.sauceUp = function () {
   if (this.sauce ==="pesto") {
     this.pie.push(2.00)
@@ -43,11 +45,11 @@ Pizza.prototype.sauceUp = function () {
     this.pie.push(1.00)
   } else {
     this.pie.push(0)
-  }
-}
-
+  };
+};
+//takes selected toppings and calculates a dollar value
 Pizza.prototype.topitUp = function() {
-  var topFee = 0
+  var topFee = 0;
   this.toppings.forEach(function(topping) {
     if (topping === "pepperoni" ){
       return  topFee +=.25
@@ -74,47 +76,43 @@ Pizza.prototype.topitUp = function() {
   } else {
       return topFee += 0
     }
-  })
+  });
   this.pie.push(topFee);
-}
-
+};
+//takes the results from that were pushed to array this.pie, and adds them together to give the user a pric for thier pizza.
 Pizza.prototype.pizzaPrice = function() {
   var zaCost = 0
   this.pie.forEach(function(pie){
 return    zaCost += pie
-  })
+  });
 this.price = zaCost.toFixed(2);
-}
-
-
-
-
-
-
+};
+//Front end
 $(document).ready(function() {
   $("form#pizza-choice").submit(function(event){
     event.preventDefault();
     var orderToppings = [];
-    $("input:checkbox[ name=p-Topping]:checked").each(function(){
-      var topping = $(this).val();
-      orderToppings.push(topping)
-    })
+      $("input:checkbox[ name=p-Topping]:checked").each(function(){
+        var topping = $(this).val();
+        orderToppings.push(topping)
+      })
     var orderCheeses = [];
-    $("input:checkbox[ name=p-Cheese]:checked").each(function(){
-      var cheese = $(this).val();
-      orderCheeses.push(cheese);
-    })
-
-    var zaOrder = new Pizza($("input:checkbox[ name=p-Size]:checked").val(), $("input:checkbox[ name=p-Sauce]:checked").val(),orderCheeses ,orderToppings, 0,[])
-
-    // alert(zaOrder.size+","+ zaOrder.sauce+","+zaOrder.cheese+","+ zaOrder.toppings );
-
-  zaOrder.sizeUp();
-  zaOrder.cheeseUp();
-  zaOrder.sauceUp();
-  zaOrder.topitUp();
-  alert(zaOrder.pie);
-  zaOrder.pizzaPrice();
-  alert(zaOrder.price);
+      $("input:checkbox[ name=p-Cheese]:checked").each(function(){
+        var cheese = $(this).val();
+        orderCheeses.push(cheese);
+      })
+    var zaOrder = new Pizza($("input:checkbox[ name=p-Size]:checked").val(), $("input:checkbox[ name=p-Sauce]:checked").val(),orderCheeses ,orderToppings, 0,[]);
+    zaOrder.sizeUp();
+    zaOrder.cheeseUp();
+    zaOrder.sauceUp();
+    zaOrder.topitUp();
+    zaOrder.pizzaPrice();
+  $("span#price-tag").text(zaOrder.price)
+  $("form#pizza-choice").hide();
+  $("div#pizza-cost").show();
+  });
+$("button#moar-pizza").click(function(){
+  $("form#pizza-choice").show();
+  $("div#pizza-cost").hide();
   });
 });
