921fb348239e981931c651b5163639320d79afc5
Marty Kovach <tyedye105@yahoo.com>
2016-10-29 09:21:13 -0700

    add the logic to the cheese pricing, that works as orginally intend.

--- js/scripts.js
+++ js/scripts.js
@@ -13,38 +13,59 @@ function Pizza(size, sauce, cheese , toppings, price, pie) {
     this.pie.push(5.99);
    } else if (this.size === "large") {
   this.pie.push(6.99);
-  };
+} else {
+  this.pie.push(0);
+}
 };
 Pizza.prototype.cheeseUp = function () {
+  var cheeseFee = 0;
   this.cheese.forEach(function(cheese) {
-    if (cheese === "smoked cheddar") {
-      return cheeseFee = 2.50
-    } else if (cheese === "pepper jack" || "cheddar") {
-      return cheeseFee =1.00
-  } else if (cheese === "mozzerella"){
-      return  cheeseFee =.50
-    }
+    if (cheese === "mozzerella"){
+      return  cheeseFee += .50
+    } else if (cheese === "pepper jack" || cheese === "cheddar") {
+      return cheeseFee += .70
+  }else if (cheese === "smoked cheddar") {
+    return cheeseFee += 1.50
+  } else {
+    return cheeseFee += 0;
+  }
   })
   this.pie.push(cheeseFee);
 };
 
-
-
-
-
-
-
-
-// Determines base price by size
-//  var pizzaPrice = function() {
-//   if (this.size === "small") {
-//     this.price =+ 4.99;
-//   } else if (pizza.size === "medium") {
-//     pizza.price =+ 5.99;
-//   } else if (pizza.size === "large") {
-//     pizza.price =+ 6.99
-//     }
+// Pizza.prototype.sauceUp = function () {
+//   if (this.sauce ==="pesto") {
+//     this.pie.push(2.00)
+//   } else if (this.sauce === "white sauce" || this.sauce === "red sauce" || this.sauce === "barbeque") {
+//     this.pie.push(.75)
+//   } else {
+//     this.pie.push(0)
+//   }
 // }
+
+// Pizza.prototype.topitUp = function() {
+//   this.toppings.forEach(function(meat) {
+//     if (meat === "pepperoni" ){
+//       return  topFee =.25
+//     } else if (meat === "sausage") {
+//       return topFee = .25
+//   } else if (meat === "spicy sausage" || meat === "chicken") {
+//     return topFee = .75
+//   } else if (meat === "chicken") {
+//     return topFee = 1.00
+//   }  else if (meat === "bacon" ) {
+//     return topFee = 1.25
+//   }
+//     else {
+//       return topFee = 0
+//     }
+//   })
+//   this.pie.push(topFee);
+// }
+
+
+
+
 
 
 $(document).ready(function() {
@@ -63,10 +84,12 @@ $(document).ready(function() {
 
     var zaOrder = new Pizza($("input:checkbox[ name=p-Size]:checked").val(), $("input:checkbox[ name=p-Sauce]:checked").val(),orderCheeses ,orderToppings, 0,[])
 
-    alert(zaOrder.cheese );
+    alert(zaOrder.size+","+ zaOrder.sauce+","+zaOrder.cheese+","+ zaOrder.toppings );
 
   zaOrder.sizeUp();
   zaOrder.cheeseUp();
+  // zaOrder.sauceUp();
+  // zaOrder.topitUp();
   alert(zaOrder.pie);
 
   });
