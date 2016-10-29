95cdfb7cd0487d47467ade3cdb044357e7428f59
Marty Kovach <tyedye105@yahoo.com>
2016-10-29 03:30:27 -0700

    add prototypes to convert size, and cheese into numbers.

--- js/scripts.js
+++ js/scripts.js
@@ -1,23 +1,50 @@
-function Pizza(size, cheese, sauce, toppings, price, pie) {
+function Pizza(size, sauce, cheese , toppings, price, pie) {
    this.size = size;
-   this.cheese = cheese;
    this.sauce = sauce;
+   this.cheese = cheese;
    this.toppings = toppings;
    this.price = 0;
    this.pie = [];
  }
-// Determines base price by size
- var pizzaPrice = function() {
-  if (pizza.size === "small") {
-    pizza.price =+ 4.99;
-  } else if (pizza.size === "medium") {
-    pizza.price =+ 5.99;
-  } else if (pizza.size === "large") {
-    pizza.price =+ 6.99
+ Pizza.prototype.sizeUp = function () {
+   if (this.size === "small") {
+     this.pie.push(4.99);
+  } else if (this.size === "medium") {
+    this.pie.push(5.99);
+   } else if (this.size === "large") {
+  this.pie.push(6.99);
+  };
+};
+Pizza.prototype.cheeseUp = function () {
+  this.cheese.forEach(function(cheese) {
+
+    if (cheese === "smoked cheddar") {
+      return  cheeseFee =1.50
+
+    } else {
+      return  cheeseFee =.50
     }
+  })
+  this.pie.push(cheeseFee);
+};
 
 
-}
+
+
+
+
+
+
+// Determines base price by size
+//  var pizzaPrice = function() {
+//   if (this.size === "small") {
+//     this.price =+ 4.99;
+//   } else if (pizza.size === "medium") {
+//     pizza.price =+ 5.99;
+//   } else if (pizza.size === "large") {
+//     pizza.price =+ 6.99
+//     }
+// }
 
 
 
@@ -49,7 +76,12 @@ $(document).ready(function() {
     })
 
     var zaOrder = new Pizza($("input:checkbox[ name=p-Size]:checked").val(), $("input:checkbox[ name=p-Sauce]:checked").val(),orderCheeses ,orderToppings, 0,[])
-    alert(zaOrder.size +" " +zaOrder.cheese+ " "+zaOrder.sauce+" "+zaOrder.toppings+ " "+ zaOrder.price);
+
+    alert(zaOrder.cheese );
+
+  zaOrder.sizeUp();
+  zaOrder.cheeseUp();
+  alert(zaOrder.pie);
 
   });
 });
